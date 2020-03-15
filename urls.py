fca39aeee22cdf84fe196ed678f6682b723a99e0
tamagoyaki-yakitamago <yuki.takeda.6e0702@outlook.jp>
2020-03-15 12:29:49 +0900

    POSTリクエストを追加

--- urls.py
+++ urls.py
@@ -5,7 +5,22 @@ api = responder.API()
 
 @api.route("/")
 class Index:
-    def on_get(self, req, res):
+    # Add headers
+    def add_headers(self, res):
         res.headers["Content-Type"] = "text/html; charset=utf-8"
+
+    # GET method
+    def on_get(self, req, res):
+        self.add_headers(res)
+
+        res.content = api.template("index.html")
+
+    # POST method
+    async def on_post(self, req, res):
+        self.add_headers(res)
+        data = await req.media()
+        
+        for i in range(len(data)):
+            print(data.get(f"entryname[{i}]"))
 
         res.content = api.template("index.html")
