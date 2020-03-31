319e9a86e2039b8da8a949f773cd0113e93f7040
tamagoyaki-yakitamago <yuki.takeda.6e0702@outlook.jp>
2020-03-31 22:32:46 +0900

    HTTPSへ切り替え

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from urls import api
 
 if __name__ == "__main__":
-    # Debug mode
-    api.run(debug=True)
-    # api.run(address="0.0.0.0", port=80, debug=True)
+    api.run(address="0.0.0.0", port=443, debug=False)
